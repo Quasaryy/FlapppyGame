14bd62cf0c659f87f469a7835992cbe7dc8ef454
Yury Lebedev <quasaryy@gmail.com>
2023-11-06 21:06:48 +0400

    Initial Commit

---
+++ FlapppyGame/FlapppyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlapppyGameApp.swift
+//  FlapppyGame
+//
+//  Created by Yury on 06.11.23.
+//
+
+import SwiftUI
+
+@main
+struct FlapppyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
